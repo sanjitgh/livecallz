58cc808c5fb9c101015c873452af45e494aac105
sanjitlearn <107295899+sanjitgh@users.noreply.github.com>
2023-07-09 12:36:47 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// toggle
+let menutoggle = document.querySelector('.toggle');
+    menutoggle.onclick = function () {
+      menutoggle.classList.toggle('active')
+    }
+
+// wow animation
+new WOW().init();
+
